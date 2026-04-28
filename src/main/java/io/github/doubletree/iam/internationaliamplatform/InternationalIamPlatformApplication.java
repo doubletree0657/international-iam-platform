95a1914975d60b491abb05ec43a8a8fa436b94a1
DoubleTree <doubletree0657@gmail.com>
2026-04-28 00:42:58 -0700

    chore: bootstrap project skeleton

---
+++ src/main/java/io/github/doubletree/iam/internationaliamplatform/InternationalIamPlatformApplication.java
@@ -0,0 +1,13 @@
+package io.github.doubletree.iam.internationaliamplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalIamPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternationalIamPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/doubletree/iam/internationaliamplatform/InternationalIamPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.doubletree.iam.internationaliamplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalIamPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
